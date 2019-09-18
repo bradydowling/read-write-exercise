aec97ef7abf9774efbf8c1831fcb2565f6e9b438
Brady Dowling <brady@amazon.com>
2019-09-18 17:47:50 -0400

    Add sock merchant

--- exercises/hackerrank/sockMerchant.js
+++ exercises/hackerrank/sockMerchant.js
@@ -1,22 +1,17 @@
-const n = 9
-
-const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
-
-function sockMerchant(n, ar) {
-  const socksByType = ar.reduce((accumulator, item) => {
-    if (!accumulator[item]) {
-      accumulator[item] = 0;
+function countPairs(n, socksArray) {
+  const socksHashMap = {};
+  let totalPairsNum = 0;
+  socksArray.forEach((sockColor) => {
+    if (!socksHashMap[sockColor]) {
+      socksHashMap[sockColor] = 1;
+      return;
     }
-    accumulator[item] = accumulator[item] + 1;
-    return accumulator;
-  }, {});
-
-  const totalPairs = Object.keys(socksByType).reduce((total, sockType) => {
-    const totalForType = socksByType[sockType];
-    return total + Math.floor(totalForType / 2);
-  }, 0);
-
-  return totalPairs;
+    totalPairsNum++;
+    delete socksHashMap[sockColor];
+  });
+  return totalPairsNum;
 }
 
-sockMerchant(n, ar);
+const n = 9;
+const ar = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+console.log(countPairs(n, ar));
